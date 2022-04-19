2a2855ea87566eec36c6f1344bcff413813b3f80
Richa <49235266+Richa-rani02@users.noreply.github.com>
2022-04-19 17:12:24 +0530

    solved question3

--- QOD/19_04_22/question3.js
+++ QOD/19_04_22/question3.js
@@ -1,1 +1,6 @@
-// Reverse the string word wise Input : welcome to neog camp ->camp neog to welcome
+// Reverse the string word wise Input : welcome to neog camp ->camp neog to welcomeconst reverseWord=(str)=>{
+    let rev=str.split(" ").reverse().join(" ");
+    return rev;
+}
+const res=reverseWord("welcome to Neog Camp");
+console.log(res);
