03919be733d0713721f8eb0c4adaa9faf38402f9
Richa <49235266+Richa-rani02@users.noreply.github.com>
2022-04-19 16:34:18 +0530

    solved 2nd question

--- QOD/19_04_22/question2.js
+++ QOD/19_04_22/question2.js
@@ -1,1 +1,57 @@
 // Given a string ,determine it its pallindrome,considering only alphanumeric characters
+
+
+// First Approach
+
+const pallindrome=(str)=>{
+    str=str.toLowerCase();
+    let i=0;
+    let j=str.length-1;
+    
+    while(i<j){
+        if(str.charAt(i)!==str.charAt(j)){
+            return false;
+        }else{
+            i++;
+            j--;
+        }
+    }
+    return true;
+    
+}
+const res=pallindrome("Abcdcba");
+console.log(res);
+
+//2nd Approach
+
+const pallindrome=(str)=>{
+    str=str.toLowerCase();
+    for(let i=0;i<str.length/2;i++){
+        if(str[i]!==str[str.length-i-1]){
+            return false;
+        }
+    }
+    return true;
+  
+}
+const res=pallindrome("Abcdcba");
+console.log(res);
+
+
+// 3rd approach using inbuilt method
+
+const pallindrome=(str)=>{
+    str=str.toLowerCase();
+    let revStr=str.split('').reverse().join('');
+    if(str!==revStr){
+        return false;
+    }
+    
+    return true;
+  
+}
+const res=pallindrome("abcdcba");
+console.log(res);
+
+
+
