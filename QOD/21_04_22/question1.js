d8578586185216871c90f00892d47140b39b6229
Richa <49235266+Richa-rani02@users.noreply.github.com>
2022-04-21 15:53:36 +0530

    Update question1.js

--- QOD/21_04_22/question1.js
+++ QOD/21_04_22/question1.js
@@ -1,1 +1,20 @@
 // 3 angles of triangle .find whether triangle is scalene, isosceles,Equilateral
+
+const findTriangle=(a,b,c)=>{
+    let output="";
+    if(a+b+c===180){
+       if(a===b===c){
+          output="Equilateral Triangle"
+       }else if(a===b || b===c || c===a){
+            output="Isosceles Triangle";
+       } else{
+           output="Scalene Triangle"
+       }
+       
+    }else{
+        output="Not a triangle";
+    }
+    return output;
+}   
+const res=findTriangle(60,60,60);
+console.log(res);
